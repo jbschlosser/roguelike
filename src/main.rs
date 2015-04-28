b3b7029a1db4b4e7718fa0a952dcdd0fceeec3cc
Joel Schlosser <joel.schlosser@gtri.gatech.edu>
2015-04-28 17:04:08 -0400

    Random starting location, empty space outside of rooms, beginning of room connection algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate rand;
 extern crate tcod;
 
-use rand::{Rand, Rng, StdRng};
+use rand::{Rng, StdRng};
 use tcod::{Console, BackgroundFlag};
 use tcod::RootInitializer;
 use tcod::input::Key::Special;
@@ -21,6 +21,7 @@ impl Location {
 
 #[derive(Copy, Clone, Debug)]
 enum Terrain {
+    Nothing,
     Floor,
     Wall
 }
@@ -68,20 +69,20 @@ impl Room {
 }
 
 impl WorldMap {
-    pub fn generate<R: Rng>(rng: &mut R, width: i32, height: i32) -> Self {
+    pub fn generate<R: Rng>(rng: &mut R, width: i32, height: i32) -> (Self, Location) {
         assert!(width > 0);
         assert!(height > 0);
 
         let mut tiles = Vec::with_capacity((width * height) as usize);
-        for i in 0..width*height {
-            tiles.push(Tile::new(Terrain::Floor));
+        for _ in 0..width*height {
+            tiles.push(Tile::new(Terrain::Nothing));
         }
 
-        let mut world = WorldMap {width: width, height: height, tiles: tiles};
+        let mut world = WorldMap { width: width, height: height, tiles: tiles };
 
         // Generate rooms.
         let mut rooms: Vec<Room> = Vec::new();
-        for i in 0..60 {
+        for _ in 0..60 {
             let room_width = rng.gen_range::<i32>(3, 15);
             let room_height = rng.gen_range::<i32>(3, 15);
             let room_x = rng.gen_range::<i32>(0, width - room_width);
@@ -95,40 +96,108 @@ impl WorldMap {
                 }
             }
             if available {
-                println!("{}x{} @ {}x{}", room_width, room_height, room_x, room_y);
+                //println!("{}x{} @ {}x{}", room_width, room_height, room_x, room_y);
                 rooms.push(room);
             } else {
-                println!("Couldn't fit it");
+                //println!("Couldn't fit it");
             }
         }
 
+        // Draw rooms.
+        let mut floor_locs = Vec::new();
+        let mut wall_locs = Vec::new();
         for room in rooms.iter() {
-            for x in room.x..room.x+room.width {
-                world.get_tile_mut(Location::new(x, room.y)).terrain = Terrain::Wall;
-                world.get_tile_mut(Location::new(x, room.y+room.height-1)).terrain =
-                    Terrain::Wall;
+            // Horizontal walls.
+            for x in room.x+1..room.x+room.width-1 {
+                let wall1 = Location::new(x, room.y);
+                let wall2 = Location::new(x, room.y+room.height-1);
+                world.get_tile_mut(wall1).terrain = Terrain::Wall;
+                world.get_tile_mut(wall2).terrain = Terrain::Wall;
+                wall_locs.push(wall1);
+                wall_locs.push(wall2);
             }
-            for y in room.y..room.y+room.height {
-                world.get_tile_mut(Location::new(room.x, y)).terrain = Terrain::Wall;
-                world.get_tile_mut(Location::new(room.x+room.width-1, y)).terrain =
-                    Terrain::Wall;
+            // Vertical walls.
+            for y in room.y+1..room.y+room.height-1 {
+                let wall1 = Location::new(room.x, y);
+                let wall2 = Location::new(room.x+room.width-1, y);
+                world.get_tile_mut(wall1).terrain = Terrain::Wall;
+                world.get_tile_mut(wall2).terrain = Terrain::Wall;
+                wall_locs.push(wall1);
+                wall_locs.push(wall2);
+            }
+            // Corners.
+            world.get_tile_mut(Location::new(room.x, room.y)).terrain = Terrain::Wall;
+            world.get_tile_mut(Location::new(room.x, room.y+room.height-1)).terrain =
+                Terrain::Wall;
+            world.get_tile_mut(Location::new(room.x+room.width-1, room.y)).terrain =
+                Terrain::Wall;
+            world.get_tile_mut(Location::new(room.x+room.width-1,
+                room.y+room.height-1)).terrain = Terrain::Wall;
+            // Floors.
+            for x in room.x+1..room.x+room.width-1 {
+                for y in room.y+1..room.y+room.height-1 {
+                    let loc = Location::new(x, y);
+                    world.get_tile_mut(loc).terrain = Terrain::Floor;
+                    floor_locs.push(loc);
+                }
             }
         }
 
-        world
+        // Draw paths between rooms.
+        for _ in 0..1 {
+            let wall1 = wall_locs[rng.gen_range::<usize>(0, wall_locs.len() - 1)];
+            let wall2 = wall_locs[rng.gen_range::<usize>(0, wall_locs.len() - 1)];
+            let path = world.find_path(wall1, wall2);
+            println!("Drawing path from {:?} to {:?}: {:?}", wall1, wall2, path);
+        }
+
+        // Pick a random floor tile to start on.
+        let starting_loc = floor_locs[rng.gen_range::<usize>(0, floor_locs.len() - 1)];
+
+        (world, starting_loc)
     }
     pub fn tiles(&self) -> TileIterator {
         TileIterator::new(&self.tiles, self.width)
     }
     pub fn get_tile(&self, loc: Location) -> &Tile {
         let index = (loc.y * self.width + loc.x) as usize;
-        assert!(index >= 0 && index < self.tiles.len());
+        assert!(index < self.tiles.len());
         &self.tiles[index]
     }
     pub fn get_tile_mut(&mut self, loc: Location) -> &mut Tile {
         let index = (loc.y * self.width + loc.x) as usize;
-        assert!(index >= 0 && index < self.tiles.len());
+        assert!(index < self.tiles.len());
         &mut self.tiles[index]
+    }
+    fn get_adjacent(&self, loc: Location) -> Vec<Location> {
+        let mut adjacent = Vec::new();
+        if loc.x > 0 { adjacent.push(Location::new(loc.x - 1, loc.y)); }
+        if loc.y > 0 { adjacent.push(Location::new(loc.x, loc.y - 1)); }
+        if loc.x < self.width - 1 { adjacent.push(Location::new(loc.x + 1, loc.y)); }
+        if loc.y < self.height - 1 { adjacent.push(Location::new(loc.x, loc.y + 1)); }
+
+        return adjacent;
+    }
+    fn find_path(&self, start: Location, end: Location) -> Option<Vec<Location>> {
+        // TODO: Implement priority-queue based A* algorithm.
+        /*let mut potential = Vec::new();
+        potential.push(vec![start]);
+        while potential.len() > 0 && start != end {
+            let next_try = potential.pop();
+            let adjacent = self.get_adjacent(start);
+            for loc in adjacent {
+                match self.get_tile(loc).terrain {
+                    Terrain::Floor => {
+                        println!("Adjacent: {:?}", loc);
+                    },
+                    _ => {}
+                }
+            }
+        }
+
+        return potential.pop();*/
+
+        return None;
     }
 }
 
@@ -162,17 +231,17 @@ impl<'a> Iterator for TileIterator<'a> {
 }
 
 fn main() {
-    let WIDTH = 80;
-    let HEIGHT = 50;
+    let width = 80;
+    let height = 50;
     let mut console = RootInitializer::new()
-        .size(WIDTH, HEIGHT)
+        .size(width, height)
         .title("Roguelike")
         .init();
 
     let mut rng = StdRng::new().unwrap();
-    let mut world = WorldMap::generate(&mut rng, WIDTH, HEIGHT);
+    let (world, starting_loc) = WorldMap::generate(&mut rng, width, height);
 
-    let mut location = Location {x: 0, y: 0};
+    let mut location = starting_loc;
     while !console.window_closed() {
         console.clear();
 
@@ -184,6 +253,9 @@ fn main() {
                 },
                 Terrain::Wall => {
                     console.put_char(location.x, location.y, '#', BackgroundFlag::Set);
+                },
+                Terrain::Nothing => {
+                    console.put_char(location.x, location.y, ' ', BackgroundFlag::Set);
                 }
             }
         }
@@ -196,15 +268,24 @@ fn main() {
         if keypress.pressed {
             let new_loc = match keypress.key {
                 Special(Escape) => break,
-                Special(Up) => Location::new(location.x, location.y - 1),
+                Special(Up) => {
+                    if location.y > 0 {
+                        Location::new(location.x, location.y - 1)
+                    } else { location }
+                },
                 Special(Down) => Location::new(location.x, location.y + 1),
-                Special(Left) => Location::new(location.x - 1, location.y),
+                Special(Left) => {
+                    if location.x > 0 {
+                        Location::new(location.x - 1, location.y)
+                    } else { location }
+                },
                 Special(Right) => Location::new(location.x + 1, location.y),
                 _ => location
             };
             match world.get_tile(new_loc).terrain {
                 Terrain::Floor => location = new_loc,
                 Terrain::Wall => {},
+                Terrain::Nothing => {}
             }
         }
     }
